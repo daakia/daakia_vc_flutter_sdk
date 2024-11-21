6b2a0f0b7d2ebd4dd6c628c74171cfa723b084b6
dpl-ashif <ashif.ali@daakia.co.in>
2024-11-21 16:30:57 +0530

    password protected

--- lib/daakia_vc_flutter_sdk.dart
+++ lib/daakia_vc_flutter_sdk.dart
@@ -6,6 +6,8 @@ import 'package:daakia_vc_flutter_sdk/screens/loading_screen.dart';
 import 'package:daakia_vc_flutter_sdk/screens/prejoin_screen.dart';
 import 'package:flutter/widgets.dart';
 
+import 'model/meeting_details_model.dart';
+
 class DaakiaVideoConferenceWidget  extends StatefulWidget{
   const DaakiaVideoConferenceWidget({required this.meetingId, required this.secretKey, this.isHost = false, super.key});
 
@@ -23,13 +25,14 @@ class _DaakiaVideoConferenceState extends State<DaakiaVideoConferenceWidget>{
   var _isLoading = false;
   var _verified = false;
   var _licenseMessage = "";
+  MeetingDetailsModel? meetingDetails;
   @override
   void initState() {
-    verifyLicense();
+    _verifyLicense();
     super.initState();
   }
 
-  void verifyLicense(){
+  void _verifyLicense(){
     setState(() {
       _isLoading = true;
     });
@@ -40,6 +43,8 @@ class _DaakiaVideoConferenceState extends State<DaakiaVideoConferenceWidget>{
     apiClient.licenceVerify(body).then((response){
       if(response.success == 1){
         _verified = response.data?.userVerified ?? false;
+        _getMeetingDetails();
+        return;
       } else {
         _verified = false;
         _licenseMessage = "License key not verified";
@@ -53,13 +58,34 @@ class _DaakiaVideoConferenceState extends State<DaakiaVideoConferenceWidget>{
       });
     });
   }
+
+  void _getMeetingDetails() {
+    setState(() {
+      _isLoading = true;
+    });
+    apiClient.getMeetingDetails(widget.meetingId, widget.secretKey).then((response){
+      if(response.success == 1){
+        meetingDetails = response.data;
+      } else {
+        _verified = false;
+        _licenseMessage = response.message ?? "Meeting details not found!";
+      }
+      setState(() {_isLoading = false;});
+    }).onError((handleError, stackStress){
+      setState(() {
+        _verified = false;
+        _isLoading = false;
+        _licenseMessage = "Something went wrong!";
+      });
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    Widget screen = LoadingScreen();
+    Widget screen = const LoadingScreen();
     if(_isLoading){
-      screen = LoadingScreen();
+      screen = const LoadingScreen();
     } else if(_verified){
-      screen = PreJoinScreen(meetingId: widget.meetingId, secretKey: widget.secretKey, isHost: widget.isHost);
+      screen = PreJoinScreen(meetingId: widget.meetingId, secretKey: widget.secretKey, isHost: widget.isHost, basicMeetingDetails: meetingDetails,);
     } else{
       screen = LicenseExpiredScreen(_licenseMessage);
     }
---
+++ lib/model/meeting_details_model.dart
@@ -0,0 +1,120 @@
+class MeetingDetailsModel {
+  String? eventMode;
+  String? eventType;
+  String? eventName;
+  String? startDate;
+  String? endDate;
+  int? duration;
+  int? isStartNow;
+  String? topic;
+  String? timeZone;
+  String? timezoneIdentifier;
+  String? subject;
+  String? chapter;
+  String? subTopic;
+  String? host;
+  String? roomUid;
+  int? conferenceStatusId;
+  int? totalMembersCount;
+  String? isPassword;
+  ConferenceStatus? conferenceStatus;
+  bool? isLobbyMode;
+  bool? isStandardPassword;
+  bool? isCommonPassword;
+
+  MeetingDetailsModel(
+      {this.eventMode,
+        this.eventType,
+        this.eventName,
+        this.startDate,
+        this.endDate,
+        this.duration,
+        this.isStartNow,
+        this.topic,
+        this.timeZone,
+        this.timezoneIdentifier,
+        this.subject,
+        this.chapter,
+        this.subTopic,
+        this.host,
+        this.roomUid,
+        this.conferenceStatusId,
+        this.totalMembersCount,
+        this.isPassword,
+        this.conferenceStatus,
+        this.isLobbyMode,
+        this.isStandardPassword,
+        this.isCommonPassword});
+
+  MeetingDetailsModel.fromJson(Map<String, dynamic> json) {
+    eventMode = json['event_mode'];
+    eventType = json['event_type'];
+    eventName = json['event_name'];
+    startDate = json['start_date'];
+    endDate = json['end_date'];
+    duration = json['duration'];
+    isStartNow = json['is_start_now'];
+    topic = json['topic'];
+    timeZone = json['time_zone'];
+    timezoneIdentifier = json['timezone_identifier'];
+    subject = json['subject'];
+    chapter = json['chapter'];
+    subTopic = json['sub_topic'];
+    host = json['host'];
+    roomUid = json['room_uid'];
+    conferenceStatusId = json['conference_status_id'];
+    totalMembersCount = json['total_members_count'];
+    isPassword = json['is_password'];
+    conferenceStatus = json['conference_status'] != null
+        ? ConferenceStatus.fromJson(json['conference_status'])
+        : null;
+    isLobbyMode = json['is_lobby_mode'];
+    isStandardPassword = json['is_standard_password'];
+    isCommonPassword = json['is_common_password'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['event_mode'] = eventMode;
+    data['event_type'] = eventType;
+    data['event_name'] = eventName;
+    data['start_date'] = startDate;
+    data['end_date'] = endDate;
+    data['duration'] = duration;
+    data['is_start_now'] = isStartNow;
+    data['topic'] = topic;
+    data['time_zone'] = timeZone;
+    data['timezone_identifier'] = timezoneIdentifier;
+    data['subject'] = subject;
+    data['chapter'] = chapter;
+    data['sub_topic'] = subTopic;
+    data['host'] = host;
+    data['room_uid'] = roomUid;
+    data['conference_status_id'] = conferenceStatusId;
+    data['total_members_count'] = totalMembersCount;
+    data['is_password'] = isPassword;
+    if (conferenceStatus != null) {
+      data['conference_status'] = conferenceStatus!.toJson();
+    }
+    data['is_lobby_mode'] = isLobbyMode;
+    data['is_standard_password'] = isStandardPassword;
+    data['is_common_password'] = isCommonPassword;
+    return data;
+  }
+}
+
+class ConferenceStatus {
+  String? title;
+
+  ConferenceStatus({title});
+
+  ConferenceStatus.fromJson(Map<String, dynamic> json) {
+    title = json['title'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['title'] = title;
+    return data;
+  }
+}
