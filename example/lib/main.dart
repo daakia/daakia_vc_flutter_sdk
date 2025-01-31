ece79f5792a68928b8740fc7bd7ef50fd8598fa1
dpl-ashif <ashif.ali@daakia.co.in>
2025-01-31 11:48:20 +0530

    1. Host verification fixes.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -37,7 +37,7 @@ class DataEntryScreen extends StatefulWidget {
 
 class _DataEntryState extends State<DataEntryScreen> {
   var licenseKey = "0D16716AFADABE17F5A42C6642CF2711ED9F59F2C89C12B2";
-  var meetingUID = "c77b66c9f3b80f87cc7f3744";
+  var meetingUID = "89fee8835be00af7a77c8db7";
   var isHost = false;
 
   @override
