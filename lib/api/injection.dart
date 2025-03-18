91eff058250d4e270ee3fbf14352cc169dd94101
Ashif Ali <ashifali3147@gmail.com>
2025-03-18 16:14:45 +0530

    fix: remove debugger & fix url

--- lib/api/injection.dart
+++ lib/api/injection.dart
@@ -1,6 +1,4 @@
-import 'package:daakia_vc_flutter_sdk/utils/constants.dart';
 import 'package:dio/dio.dart';
-import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 import 'api_client.dart';
 
@@ -8,25 +6,6 @@ final apiClient = RestClient(setDio());
 
 Dio setDio() {
   final dio = Dio();
-  dio.options.baseUrl = Constant.RELEASE
-      ? 'https://api.daakia.co.in/v2.0/'
-      : 'https://stag-api.daakia.co.in/v2.0/';
-  dio.interceptors.add(PrettyDioLogger(
-      requestHeader: true,
-      requestBody: true,
-      responseBody: true,
-      responseHeader: false,
-      error: true,
-      compact: true,
-      maxWidth: 90,
-      enabled: !Constant.RELEASE,
-      filter: (options, args) {
-        // don't print requests with uris containing '/posts'
-        if (options.path.contains('/posts')) {
-          return false;
-        }
-        // don't print responses with unit8 list data
-        return !args.isResponse || !args.hasUint8ListData;
-      }));
+  dio.options.baseUrl = 'https://api.daakia.co.in/v2.0/';
   return dio;
 }
