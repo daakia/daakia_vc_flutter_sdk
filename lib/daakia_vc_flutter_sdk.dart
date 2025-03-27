ef8b293197802ac861697862c1dacc7f1b08314f
Ashif Ali <ashifali3147@gmail.com>
2025-03-27 18:18:28 +0530

    feat: add abstraction layer for API calls

--- lib/daakia_vc_flutter_sdk.dart
+++ lib/daakia_vc_flutter_sdk.dart
@@ -8,31 +8,36 @@ import 'package:flutter/widgets.dart';
 
 import 'model/meeting_details_model.dart';
 
-class DaakiaVideoConferenceWidget  extends StatefulWidget{
-  const DaakiaVideoConferenceWidget({required this.meetingId, required this.secretKey, this.isHost = false, super.key});
+class DaakiaVideoConferenceWidget extends StatefulWidget {
+  const DaakiaVideoConferenceWidget(
+      {required this.meetingId,
+      required this.secretKey,
+      this.isHost = false,
+      super.key});
 
   final String meetingId;
   final String secretKey;
   final bool isHost;
+
   @override
   State<StatefulWidget> createState() {
     return _DaakiaVideoConferenceState();
   }
-
 }
 
-class _DaakiaVideoConferenceState extends State<DaakiaVideoConferenceWidget>{
+class _DaakiaVideoConferenceState extends State<DaakiaVideoConferenceWidget> {
   var _isLoading = false;
   var _verified = false;
   var _licenseMessage = "";
   MeetingDetailsModel? meetingDetails;
+
   @override
   void initState() {
     _verifyLicense();
     super.initState();
   }
 
-  void _verifyLicense(){
+  void _verifyLicense() {
     setState(() {
       _isLoading = true;
     });
@@ -40,53 +45,65 @@ class _DaakiaVideoConferenceState extends State<DaakiaVideoConferenceWidget>{
       "secret_key": widget.secretKey,
       "meeting_uid": widget.meetingId
     };
-    apiClient.licenceVerify(body).then((response){
-      if(response.success == 1){
-        _verified = response.data?.userVerified ?? false;
-        _getMeetingDetails();
-        return;
-      } else {
-        _verified = false;
-        _licenseMessage = response.message ?? "License key not verified";
-      }
-      setState(() {_isLoading = false;});
-    }).onError((handleError, stackStress) {
-      setState(() {
-        _verified = false;
-        _isLoading = false;
-        _licenseMessage = "Something went wrong!";
-      });
-    });
+    networkRequestHandler(
+        apiCall: () => apiClient.licenceVerify(body),
+        onSuccess: (data) {
+          setState(() {
+            _verified = data?.userVerified ?? false;
+            if (_verified) {
+              _getMeetingDetails();
+            } else {
+              _licenseMessage = "License key not verified.";
+            }
+            _isLoading = false;
+          });
+        },
+        onError: (message) {
+          setState(() {
+            _verified = false;
+            _isLoading = false;
+            _licenseMessage = message;
+          });
+        });
   }
 
   void _getMeetingDetails() {
     setState(() {
       _isLoading = true;
     });
-    apiClient.getMeetingDetails(widget.meetingId, widget.secretKey).then((response){
-      if(response.success == 1){
-        meetingDetails = response.data;
-      } else {
-        _verified = false;
-        _licenseMessage = response.message ?? "Meeting details not found!";
-      }
-      setState(() {_isLoading = false;});
-    }).onError((handleError, stackStress){
-      setState(() {
-        _verified = false;
-        _isLoading = false;
-        _licenseMessage = "Something went wrong!";
-      });
-    });
+
+    networkRequestHandler(
+      apiCall: () =>
+          apiClient.getMeetingDetails(widget.meetingId, widget.secretKey),
+      onSuccess: (response) {
+        meetingDetails = response;
+        setState(() {
+          _isLoading = false;
+        });
+      },
+      onError: (message) {
+        setState(() {
+          _verified = false;
+          _isLoading = false;
+          _licenseMessage = message;
+        });
+      },
+    );
   }
+
   @override
   Widget build(BuildContext context) {
     Widget screen = const LoadingScreen();
-    if(_isLoading){
+    if (_isLoading) {
       screen = const LoadingScreen();
-    } else if(_verified){
-      screen = PreJoinScreen(meetingId: widget.meetingId, secretKey: widget.secretKey, isHost: widget.isHost, basicMeetingDetails: meetingDetails,);
-    } else{
+    } else if (_verified) {
+      screen = PreJoinScreen(
+        meetingId: widget.meetingId,
+        secretKey: widget.secretKey,
+        isHost: widget.isHost,
+        basicMeetingDetails: meetingDetails,
+      );
+    } else {
       screen = LicenseExpiredScreen(_licenseMessage);
     }
     return screen;
