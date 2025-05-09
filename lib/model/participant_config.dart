da04a3e86a851258395cfbabc527757e75b10127
Ashif Ali <ashifali3147@gmail.com>
2025-05-09 10:58:28 +0530

    docs: add documentation for ParticipantNameConfig class

--- lib/model/participant_config.dart
+++ lib/model/participant_config.dart
@@ -8,6 +8,10 @@ class ParticipantNameConfig {
   /// This value is only respected if [name] is not empty.
   final bool isEditable;
 
+  /// If `name` is provided inside [ParticipantNameConfig] and is non-empty,
+  /// then the `isEditable` flag controls whether the user can modify it in the pre-join screen.
+  ///
+  /// If `name` is not provided or is empty, the name field will always be editable.
   const ParticipantNameConfig({
     this.name,
     this.isEditable = true,
