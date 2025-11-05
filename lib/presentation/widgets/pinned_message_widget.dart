3b03823695fe01f7fee40b8342c05b647f34c2ee
Ashif Ali <84494015+ashifali3147@users.noreply.github.com>
2025-11-05 13:09:53 +0530

    feat: added onTap event for navigating to pinned message location in pinned_message_widget.dart

--- lib/presentation/widgets/pinned_message_widget.dart
+++ lib/presentation/widgets/pinned_message_widget.dart
@@ -7,62 +7,67 @@ class PinnedMessageWidget extends StatelessWidget {
   final String? name;
   final String message;
   final VoidCallback? onPinPressed;
+  final VoidCallback? onPinNavigatePressed;
 
   const PinnedMessageWidget({
     super.key,
     required this.name,
     required this.message,
     this.onPinPressed,
+    this.onPinNavigatePressed
   });
 
   @override
   Widget build(BuildContext context) {
     final initials = Utils.getInitials(name);
     final bgColor = Utils.generateUniqueColorFromInitials(name ?? "U");
-    return Card(
-      color: const Color(0xFF303030),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8.0),
-      ),
-      elevation: 0,
-      margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            // User initials
-            InitialsCircle(
-              initials: initials,
-              size: 34,
-              backgroundColor: bgColor,
-            ),
-            const SizedBox(width: 10),
-
-            // Message text (single line, ellipsis)
-            Expanded(
-              child: Text(
-                message,
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 14,
+    return GestureDetector(
+      onTap: onPinNavigatePressed,
+      child: Card(
+        color: const Color(0xFF303030),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        elevation: 0,
+        margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // User initials
+              InitialsCircle(
+                initials: initials,
+                size: 34,
+                backgroundColor: bgColor,
+              ),
+              const SizedBox(width: 10),
+      
+              // Message text (single line, ellipsis)
+              Expanded(
+                child: Text(
+                  message,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                  ),
                 ),
               ),
-            ),
-
-            // Pinned icon button
-            IconButton(
-              onPressed: onPinPressed,
-              icon: const Icon(
-                Icons.push_pin_outlined,
-                color: Colors.white,
-                size: 20,
+      
+              // Pinned icon button
+              IconButton(
+                onPressed: onPinPressed,
+                icon: const Icon(
+                  Icons.push_pin_outlined,
+                  color: Colors.white,
+                  size: 20,
+                ),
+                tooltip: "Unpin message",
               ),
-              tooltip: "Unpin message",
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
